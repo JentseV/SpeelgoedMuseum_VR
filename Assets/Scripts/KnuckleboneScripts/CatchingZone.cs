7b8b59e31cd4fd885eeb1b8f6650e7220db41432
NielsDeBacker01 <niels.debacker01@student.ap.be>
2023-12-13 10:21:06 +0100

    finish knucklebone

--- Assets/Scripts/KnuckleboneScripts/CatchingZone.cs
+++ Assets/Scripts/KnuckleboneScripts/CatchingZone.cs
@@ -11,9 +11,11 @@ public class CatchingZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ShootRaycastUp();
         if (caughtObject != null)
         {
-            if((Mathf.Abs(gameObject.transform.rotation.eulerAngles.x) < 30 ||  Mathf.Abs(gameObject.transform.rotation.eulerAngles.x) > 330) && (Mathf.Abs(gameObject.transform.rotation.eulerAngles.z) < 30 + zOffset||  Mathf.Abs(gameObject.transform.rotation.eulerAngles.z) > 330 + zOffset) )
+            if((Mathf.Abs(gameObject.transform.rotation.eulerAngles.x) < 30 ||  Mathf.Abs(gameObject.transform.rotation.eulerAngles.x) > 330) 
+            && (Mathf.Abs(gameObject.transform.rotation.eulerAngles.z) < 30 + zOffset||  Mathf.Abs(gameObject.transform.rotation.eulerAngles.z) > 330 + zOffset) )
             {
                 HoldCaughtObject();
             }
@@ -24,7 +26,6 @@ public class CatchingZone : MonoBehaviour
                 Rigidbody rb = caughtObject.GetComponent<Rigidbody>();
                 rb.velocity = Vector3.zero;
                 caughtObject = null;
-                Debug.Log("Dropped object");
             }
         }
 
@@ -38,11 +39,38 @@ public class CatchingZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("catchable") && (caughtObject == null)) //add check for grab cooldown
+        if (other.CompareTag("catchable") && (caughtObject == null) && (gameObject.transform.position.y < other.transform.position.y))
         {
             caughtObject = other.gameObject;
             caughtObject.transform.parent = transform;
             Debug.Log("caught object");
+        }
+    }
+
+    public void TryCatch(GameObject other)
+    {
+
+    }
+        public float raycastDistance = 10f;
+
+    void ShootRaycastUp()
+    {
+        // Get the position of the GameObject this script is attached to
+        Vector3 raycastOrigin = transform.position;
+
+        // Shoot a raycast in the upward direction from the GameObject's position
+        RaycastHit hit;
+        if (Physics.Raycast(raycastOrigin, Vector3.up, out hit, raycastDistance))
+        {
+            // The ray hit something
+            Debug.Log("Ray hit: " + hit.collider.gameObject.name);
+
+            // You can perform additional actions here, based on what the ray hit
+        }
+        else
+        {
+            // The ray did not hit anything
+            Debug.Log("Ray did not hit anything");
         }
     }
 }
